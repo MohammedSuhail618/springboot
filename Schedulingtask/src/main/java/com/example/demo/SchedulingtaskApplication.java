74ee725f2d98d8c0c506517493c2a19d81e21a16
MohammedSuhail618 <suhailshaik2024@gmail.com>
2025-02-11 14:37:19 +0530

    Day 9

---
+++ Schedulingtask/src/main/java/com/example/demo/SchedulingtaskApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingtaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulingtaskApplication.class, args);  // Corrected the class name
+    }
+}
