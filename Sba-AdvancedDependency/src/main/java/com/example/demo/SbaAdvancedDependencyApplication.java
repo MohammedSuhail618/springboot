9859031f0738ae2a6fc478a0dae277ce90a2d32d
MohammedSuhail618 <suhailshaik2024@gmail.com>
2025-02-12 15:38:47 +0530

    Day10

---
+++ Sba-AdvancedDependency/src/main/java/com/example/demo/SbaAdvancedDependencyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbaAdvancedDependencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbaAdvancedDependencyApplication.class, args);
+	}
+
+}
